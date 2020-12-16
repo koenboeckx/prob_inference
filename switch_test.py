23916704f94d4c4601f0852e83e5fe92fde88654
Koen Boeckx <koen_boeckx@hotmail.com>
2020-12-16 10:04:28 +0100

    (small) changes to ma-gym Switch

--- switch_test.py
+++ switch_test.py
@@ -9,6 +9,7 @@ import gym
 import ma_gym
 
 class Switch:
+    "No longer needed because Switch has fully-observable version ('Switch2-v1')"
     def __init__(self):
         self.env = gym.make('Switch2-v0')
         self.action_space = self.env.action_space
@@ -31,19 +32,26 @@ class Switch:
     def close(self):
         self.env.close()
     
-
-
-env = Switch()
+#env = Switch()
+env = gym.make('Switch2-v1') # v1 makes switch fully-observable
 done_n = [False for _ in range(env.n_agents)]
 ep_reward = 0
 
 obs_n = env.reset()
-"""
-while not all(done_n):
+print(obs_n)
+
+#while not all(done_n):
+actions2 = [3, 0, 0, 1, 2, 4]
+actions1 = [4, 4, 4, 4, 4, 4]
+for actions in zip(actions1, actions2):
     env.render()
-    obs_n, reward_n, done_n, info = env.step(env.action_space.sample())
+    print(f'actions = {actions}')
+    obs_n, reward_n, done_n, info = env.step(actions)
+    print(obs_n)
     ep_reward += sum(reward_n)
     time.sleep(1)
-print(ep_reward)
-"""
 env.close()
+
+def learn_transition_table(env):
+    # ignore agent 2
+    pass
